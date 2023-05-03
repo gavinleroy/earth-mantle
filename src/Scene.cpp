934875b3b108dd69d9fbd13e018995661126781a
rasilu <rasilu93@gmail.com>
2023-05-04 00:43:59 +0200

    Working UI. Can load different piplines with key inputs.

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -22,23 +22,48 @@ void Scene::InitRenderer(vtkSmartPointer<vtkRenderer> renderer)
 
 void Scene::InitUI(vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor)
 {
-    /* TODO */
+    // TODO: create GUI with elements like buttons, sliders, etc ...
+
+    // print avilable input options
+    std::cout << "Available input options:" << std::endl;
+    std::cout << "l: load LIConvolution pipeline" << std::endl;
+    std::cout << "t: load Tube pipeline" << std::endl;
+    std::cout << "v: load Volumes pipeline" << std::endl;
+    //std::cout << "w: toggle wireframe" << std::endl;
+    //std::cout << "s: step forward" << std::endl;
+    //std::cout << "b: step backward" << std::endl;
 }
 
 void Scene::ProcessInput(char input)
 {
-    if (input == 'v') {
-        pipelines.push_back(std::move(std::make_unique<Volumes>()));
+    // TODO: only load the pipeline if it is not already loaded
+    // TODO: only render one pipeline at a time
+    if (input == 'l') {
+        pipelines.push_back(std::move(std::make_unique<LIConvolution>()));
         pipelines.back()->ConnectToScene(this->renderer);
     }
     if (input == 't') {
         pipelines.push_back(std::move(std::make_unique<Tube>()));
         pipelines.back()->ConnectToScene(this->renderer);
     }
-    if (input == 'l') {
-        pipelines.push_back(std::move(std::make_unique<LIConvolution>()));
+    if (input == 'v') {
+        pipelines.push_back(std::move(std::make_unique<Volumes>()));
         pipelines.back()->ConnectToScene(this->renderer);
     }
+    if (input == 'w') {
+        // add wireframe
+        // -- I think this should be loaded when scene is initialized (Rasmus)
+    }
+
+    // TODO: add contols for time stepping
+    if (input == 's') {
+        // step forward
+    }
+    if (input == 'b') {
+        // step backward
+    }
+
+    // add more controls here if needed ...
 }
 
 void Scene::Update(double dt, double t)
