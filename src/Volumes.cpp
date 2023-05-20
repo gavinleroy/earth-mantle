2d3ee631d39d728122faa0f1e0020baf3fdd4db8
Raphael Winkler <raphael.winkler@forty2.ch>
2023-05-20 19:34:17 +0200

    - Contour in Volume experiments

--- src/Volumes.cpp
+++ src/Volumes.cpp
@@ -2,15 +2,81 @@
 #include <vtkAssignAttribute.h>
 #include <vtkDataSetAttributes.h>
 
+
+void AddSegmentToCTF(
+        const vtkSmartPointer<vtkColorTransferFunction>& ctf,
+        double from, double r1, double g1, double b1,
+        double to, double r2, double g2, double b2,
+        int nr_contours = 2, double thickness = 25
+        ) {
+//            -1100, 0, 0, 1,
+//            -150,  0, 1, 1
+    double r_step = (r2-r1)/nr_contours; // 0
+    double g_step = (g2-g1)/nr_contours; // 1
+    double b_step = (b2-b1)/nr_contours; // 0
+
+    double eps;
+    if (from > 0) {
+        eps = 1e-5;
+    } else {
+        eps = 1e-5;
+    }
+
+    double step_size = (to - from)/nr_contours;
+    ctf->AddRGBPoint(
+            from,
+            r1,
+            g1,
+            b1
+    );
+    for (int i = 1; i < nr_contours; i++) {
+        ctf->AddRGBPoint(
+                from + i * step_size - thickness/2 - eps,
+                r1 + r_step * i,
+                g1 + g_step * i,
+                b1 + b_step * i
+        );
+        ctf->AddRGBPoint(
+                from + i * step_size - thickness/2,
+                0,
+                0,
+                0
+        );
+        ctf->AddRGBPoint(
+                from + i * step_size + thickness/2,
+                0,
+                0,
+                0
+        );
+        ctf->AddRGBPoint(
+                from + i * step_size + thickness/2 + eps,
+                r1 + r_step * i,
+                g1 + g_step * i,
+                b1 + b_step * i
+                );
+    }
+//    eps = -0.1;
+//    ctf->AddRGBPoint(-1100, 0., 0., 1.);
+//    ctf->AddRGBPoint(-150, 0., 1., 1.);
+//    ctf->AddRGBPoint(-75, 0.5, 1., 1.);
+//    ctf->AddRGBPoint(-74.99, 0., 0., 0.);
+//    ctf->AddRGBPoint(-50.0, 0., 0., 0.);
+//    ctf->AddRGBPoint(-49.99, 0.65, 1., 1.);
+//    ctf->AddRGBPoint(0, 1., 1., 1.);
+//    ctf->AddRGBPoint(150, 1., 1., 0.);
+//    ctf->AddRGBPoint(1100, 1.0, 0., 0.);
+}
+
 Volumes::Volumes()
     : Resample::Resample()
     , mVolume(vtkSmartPointer<vtkVolume>::New())
 {
     MantleIO::MantleAttr property  = MantleIO::MantleAttr::TempAnom;
     auto                 resampler = Resample::GetResampled();
+    auto data = MantleIO::Mantle::GetCurrentStream();
 
     vtkNew<vtkAssignAttribute> assignAttribute;
-    assignAttribute->SetInputConnection(resampler->GetOutputPort());
+    assignAttribute->SetInputConnection(data->GetOutputPort());
     assignAttribute->Assign(property.c_str(), vtkDataSetAttributes::SCALARS,
                             vtkAssignAttribute::POINT_DATA);
 
@@ -26,12 +92,30 @@ Volumes::Volumes()
     colorTransferFunction->SetColorSpaceToRGB();
     colorTransferFunction->SetScaleToLinear();
     colorTransferFunction->AdjustRange(range);
-    colorTransferFunction->AddRGBPoint(-1100, 0., 0., 1.);
-    colorTransferFunction->AddRGBPoint(-150, 0., 1., 1.);
-    colorTransferFunction->AddRGBPoint(0, 1., 1., 1.);
-    colorTransferFunction->AddRGBPoint(150, 1., 1., 0.);
-    colorTransferFunction->AddRGBPoint(1100, 1.0, 0., 0.);
-
+    AddSegmentToCTF(
+            colorTransferFunction,
+            -1100,0, 0, 1,
+            -150, 0, 1, 1,
+            3, 20
+            );
+    AddSegmentToCTF(
+            colorTransferFunction,
+            -150,0, 1, 1,
+            0, 1, 1, 1,
+            4, 20
+    );
+    AddSegmentToCTF(
+            colorTransferFunction,
+            0,1, 1, 1,
+            150, 1, 1, 0,
+            4, 20
+    );
+    AddSegmentToCTF(
+            colorTransferFunction,
+            150,1, 1, 0,
+            1100, 1, 0, 0,
+            3, 20
+    );
     // Create a piecewise function
     vtkNew<vtkPiecewiseFunction> opacityTransferFunction;
     opacityTransferFunction->AddPoint(-1100, 0.8);
--- src/Volumes.h
+++ src/Volumes.h
@@ -6,46 +6,6 @@
 #include <vtkVolumeProperty.h>
 #include <vtkPlane.h>
 #include <vtkSmartVolumeMapper.h>
-// #include <vtkPolyDataMapper.h>
-// #include <vtkActor.h>
-// #include <vtkProperty.h>
-// #include <vtkTexture.h>
-// #include <vtkRenderer.h>
-// #include <vtkNetCDFCFReader.h>
-// #include <vtkNetCDFReader.h>
-// #include <vtkPiecewiseFunction.h>
-// #include <vtkColorTransferFunction.h>
-// #include <vtkVolumeProperty.h>
-// #include <vtkDataArraySelection.h>
-// #include <vtkAssignAttribute.h>
-// #include <vtkSmartVolumeMapper.h>
-// #include <vtkVolumeMapper.h>
-// #include <vtkVolume.h>
-// #include <vtkStructuredGrid.h>
-// #include <vtkResampleToImage.h>
-// #include <vtkDataArrayCollection.h>
-// #include <vtkImageData.h>
-// #include <vtkPointData.h>
-// #include <vtkDoubleArray.h>
-// #include <vtkDataSetMapper.h>
-// #include <vtkLookupTable.h>
-// #include <vtkStringArray.h>
-// #include <vtkAssignAttribute.h>
-// #include <vtkCellData.h>
-// #include <vtkStructuredGrid.h>
-// #include <vtkPlane.h>
-// #include <vtkCutter.h>
-// #include <vtkClipPolyData.h>
-// #include <vtkClipDataSet.h>
-// #include <vtkStructuredGridGeometryFilter.h>
-// #include <vtkPolyData.h>
-// #include <vtkRectilinearGrid.h>
-// #include <vtkCellDataToPointData.h>
-// #include <vtkFloatArray.h>
-// #include <vtkFixedPointVolumeRayCastMapper.h>
-// #include <vtkThreshold.h>
-// #include <vtkUnstructuredGrid.h>
-
 #include "Pipeline.h"
 #include "Resample.h"
 
